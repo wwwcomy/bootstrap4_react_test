f8302c0993ca4482235459229ab98d684e8810ab
wwwcomy <wwwcomy@gmail.com>
2017-08-24 15:36:23 +0800

    Change 2 links...

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -3,10 +3,10 @@ import React from 'react';
 export default class Header extends React.Component {
 	 render() {return (
       <nav className="navbar navbar-inverse">
-        <a className="navbar-brand" href="#">Project name</a>
+        <a className="navbar-brand" href="/main">Project name</a>
         <ul className="nav navbar-nav">
           <li className="nav-item active">
-            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+            <a className="nav-link" href="/main">Home <span className="sr-only">(current)</span></a>
           </li>
           <li className="nav-item">
             <a className="nav-link" href="#">About</a>
