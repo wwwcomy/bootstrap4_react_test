6ad2b6e620067d18ba6d2ebbdc6589932425925f
wwwcomy <wwwcomy@gmail.com>
2017-08-26 22:58:09 +0800

    Add 2 buttons for hiding the right-wrong mark and re-generate questions.

--- src/js/components/maths/Simple.js
+++ src/js/components/maths/Simple.js
@@ -10,9 +10,18 @@ export default class Simple extends React.Component {
     super();
     this.cancelEdit = this.cancelEdit.bind(this);
     this.check = this.check.bind(this);
+    this.hide = this.hide.bind(this);
+    this.reGenerate = this.reGenerate.bind(this);
     this.crumbs=[{
       key : 'Simple Maths'
     }];
+    this.state = {
+      questionCount:0
+    };
+  }
+
+  componentDidMount(){
+    this.setState({questionCount:10});
   }
 
   cancelEdit(e) {
@@ -26,10 +35,22 @@ export default class Simple extends React.Component {
       this.refs["ex"+i].setState({"iconShow":true});
     }
   }
+  hide(e){
+    e.preventDefault();
+    for(let i=0;i<10;i++){
+      this.refs["ex"+i].setState({"iconShow":false});
+    }
+  }
+  reGenerate(e){
+    e.preventDefault();
+    this.setState({questionCount:0});
+    setTimeout(function(){this.setState({questionCount:10})}.bind(this),100);
+    // this.componentDidMount();
+  }
 
   render() {
     let inputQuestionArr = [];
-    for(let i=0;i<10;i++){
+    for(let i=0;i<this.state.questionCount;i++){
       inputQuestionArr.push(
               <div key={'outerDiv'+i}>
                 <div className="form-group">
@@ -47,7 +68,7 @@ export default class Simple extends React.Component {
           <div className="col-md-8">
             <div className="panel panel-default">
               <div className="panel-heading">
-                <h3 className="card-title">Plese calculate the result of the following questions.</h3>
+                <h3 className="card-title">Please calculate the results of the following questions.</h3>
               </div>
               <div className="panel-body">
                 <form className="form-horizontal">
@@ -60,8 +81,11 @@ export default class Simple extends React.Component {
           <div className="col-md-4">
             <div className="panel panel-default">
               <div className='panel-body'>
+                <p>The range of the number is from 0 to 10.</p>
                 <p>Check the result!</p>
                 <button type="button" className="btn btn-success btn-block" onClick = {this.check}>Check</button>
+                <button type="button" className="btn btn-info btn-block" onClick = {this.hide}>Hide Mark</button>
+                <button type="button" className="btn btn-danger btn-block" onClick = {this.reGenerate}>Re-Generate Questions</button>
               </div>
             </div>
           </div>
